7a2cbe9a70ff2b3d612cdbc0bcab01be64fab261
kimchangh <rlackdgks22@naver.com>
2021-09-17 11:03:24 +0900

    가가가\

---
+++ js/jq2.js
@@ -0,0 +1,21 @@
+// Can also be used with $(document).ready()
+$(window).ready(function() {
+
+
+
+
+
+  
+    $('.flex0').flexslider({
+      animation: "slide",
+      controlNav: "thumbnails"
+    });
+
+  });
+
+
+  $(window).ready(function() {
+    $('.flexslider').flexslider({
+      animation: "slide"
+    });
+  });
